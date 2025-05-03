3cfc1ea8b93233224ba630c148f7b85c56632d80
Jay Dunn <jaygeorgedunn@gmail.com>
2025-05-02 17:08:23 -0700

    deploy again

--- credentials/aws.js
+++ credentials/aws.js
@@ -1,4 +1,3 @@
-const credentials = [process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, "us-east-2"]
-
+const credentials = [process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.AWS_REGION]
 
 export default credentials
---
+++ credentials/emailjs.js
@@ -0,0 +1,3 @@
+const credentials = [process.env.EMAILSJS_SERVICEID, process.env.EMAILJS_TEMPLATEID, process.env.EMAILJS_PUBLICAPI]
+
+export default credentials
